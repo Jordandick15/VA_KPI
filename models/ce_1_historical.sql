3c66f2ea7fb3f9424d66fc7a82ac40bed8ec335a
jordan@plpfunds.com <jordan@plpfunds.com>
2025-11-28 21:31:49 +0000

    ce_1 successful run

--- models/ce_1_historical.sql
+++ models/ce_1_historical.sql
@@ -1,28 +1,23 @@
-{{ config(
-    materialized = 'table',
-    database = 'CE_AUTOMATION',
-    schema   = 'PUBLIC',
-    alias    = 'CE_1_HISTORICAL'
-) }}
+{{ config(materialized='table') }}
 
 WITH base AS (
-    SELECT
-        SYMBOL,
-        SEGMENT_NAME,
-        PARTIAL_PERIOD_FLAG,
-        PERIOD_TYPE,
-        PERIOD,
-        PERIOD_START_DT::DATE AS PERIOD_START_DT,
-        PTD_END_DT::DATE      AS PTD_END_DT,
-        YA_PERIOD,
-        PTD_SPEND_USD_YOY,
-        PTD_TRANS_YOY,
-        VERSION,
-        TRANS_COUNT,
-        SPEND_AMOUNT_USD
-    FROM {{ source('ce_usa1', 'PERIOD_SYM_SEG_EMAX') }}
-    WHERE SYMBOL IS NOT NULL
-      AND PERIOD_TYPE NOT IN ('HALF', 'WEEK', 'MONTH')
+  SELECT
+      SYMBOL,
+      SEGMENT_NAME,
+      PARTIAL_PERIOD_FLAG,
+      PERIOD_TYPE,
+      PERIOD,
+      PERIOD_START_DT::DATE AS PERIOD_START_DT,
+      PTD_END_DT::DATE      AS PTD_END_DT,
+      YA_PERIOD,
+      PTD_SPEND_USD_YOY,
+      PTD_TRANS_YOY,
+      VERSION,
+      TRANS_COUNT,
+      SPEND_AMOUNT_USD
+  FROM {{ source('ce_usa1', 'PERIOD_SYM_SEG_EMAX') }}
+  WHERE SYMBOL IS NOT NULL
+    AND PERIOD_TYPE NOT IN ('HALF','WEEK','MONTH')
 )
 
 SELECT *
